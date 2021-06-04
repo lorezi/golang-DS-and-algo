e2eda79c635506fd436d0d27140f55d4be1e4e48
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-04 10:33:15 +0100

    adds sequential search

--- week-one/array-interview-questions/main.go
+++ week-one/array-interview-questions/main.go
@@ -11,8 +11,21 @@ func sumList(data []int) int {
 	return sum
 }
 
+// Sequential Search ---> Write a method, which will search a list for some given value.
+func sequentialSearch(list []int, val int) string {
+	for _, v := range list {
+		if v == val {
+			return fmt.Sprintf("Found the value in the list %d", val)
+		}
+	}
+	return fmt.Sprintf("Value not found %d", val)
+}
+
 func main() {
 	data := []int{1, 4, 5, 3, 6, 9}
 	sum := sumList(data)
 	fmt.Println("The sum of the list is: ", sum)
+
+	s := sequentialSearch(data, 9)
+	fmt.Println(s)
 }
