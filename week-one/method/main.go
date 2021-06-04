6f461d8d856860a8bb8fe6be9659a7a0b3a3164d
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-04 09:30:28 +0100

    adds pass by value and pass by pointer method

--- week-one/method/main.go
+++ week-one/method/main.go
@@ -17,6 +17,16 @@ func (r Rect) Perimeter() float64 {
 	return 2 * (r.width + r.height)
 }
 
+type MyInt int
+
+func (data MyInt) increment1() {
+	data = data + 1
+}
+
+func (data *MyInt) increment2() {
+	*data = *data + 1
+}
+
 func main() {
 	r := Rect{width: 10, height: 10}
 	fmt.Println("Area: ", r.Area())
@@ -25,4 +35,11 @@ func main() {
 	ptr := &Rect{width: 10, height: 5}
 	fmt.Println("Area: ", ptr.Area())
 	fmt.Println("Perimeter: ", ptr.Perimeter())
+
+	var data MyInt = 1
+	fmt.Println("value before increment1() call: ", data)
+	data.increment1()
+	fmt.Println("value after increment1call() call : ", data)
+	data.increment2()
+	fmt.Println("value after increment2() call : ", data)
 }
