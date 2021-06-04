8941a3311b96a9608d4727a42ec8f59a3997db45
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-04 11:38:31 +0100

    adds rotate array by k number of times

--- week-one/array-interview-questions/main.go
+++ week-one/array-interview-questions/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Sum List --> Write a method that will return the sum of all the elements of the integer list, given list as an input argument.
 func sumList(data []int) int {
@@ -21,11 +23,64 @@ func sequentialSearch(list []int, val int) string {
 	return fmt.Sprintf("Value not found %d", val)
 }
 
+// Binary Search function in a sorted list
+func BinarySearch(list []int, val int) bool {
+	size := len(list)
+	var mid int
+	low := 0
+	high := size - 1
+
+	for low <= high {
+		// find the mid index in the list
+		mid = low + (high-low)/2
+		if list[mid] == val {
+			return true
+		} else {
+			if list[mid] < val { // search right because the value is greater than the middle value
+				low = mid + 1
+			} else { // search left because the value is smaller than the middle value
+				high = mid - 1
+			}
+		}
+	}
+	return false
+}
+
+/*
+
+RotateArray function rotates a list in k number of times
+Analysis: Rotating list is done in two parts trick. In the first part, we reverse elements of list first half and then second half
+*/
+
+func RotateArray(data []int, k int) {
+	size := len(data)
+	ReverseArray(data, 0, k-1)
+	ReverseArray(data, k, size-1)
+	ReverseArray(data, 0, size-1)
+}
+
+// ReverseArray function takes list of data, start and end
+func ReverseArray(data []int, start int, end int) {
+	i := start
+	j := end
+
+	for i < j {
+		// swapping ===> [i][j] = [j][i]
+		data[i], data[j] = data[j], data[i]
+		i++
+		j--
+	}
+}
+
 func main() {
 	data := []int{1, 4, 5, 3, 6, 9}
+
 	sum := sumList(data)
 	fmt.Println("The sum of the list is: ", sum)
 
 	s := sequentialSearch(data, 9)
 	fmt.Println(s)
+
+	b := BinarySearch(data, 9)
+	fmt.Println(b)
 }
